a35307662941280c814ad4d97879aacf893906ee
Damian Mooyman <damian.mooyman@isobar.com>
2019-10-03 16:49:56 +1300

    Use proc_open for proc runner as well

--- src/Process/ProcBackgroundRunner.php
+++ src/Process/ProcBackgroundRunner.php
@@ -2,6 +2,7 @@
 
 namespace TractorCow\WebConsole\Process;
 
+use Exception;
 use SilverStripe\ORM\ValidationException;
 use SilverStripe\Security\Security;
 use TractorCow\WebConsole\Model\BackgroundTask;
@@ -13,6 +14,7 @@ class ProcBackgroundRunner implements BackgroundRunner
      * @param string $path
      * @return BackgroundTask
      * @throws ValidationException
+     * @throws Exception
      */
     public function run($command, $path)
     {
@@ -27,18 +29,37 @@ class ProcBackgroundRunner implements BackgroundRunner
 
         // Build inner command to offload responsibility to the dev task
         $cliScriptPath = BASE_PATH . '/vendor/silverstripe/framework/cli-script.php';
-        $shellCMD = sprintf(
-            "%s %s dev/tasks/RunWebconsoleTask task=%d",
+        $runnerCommand = sprintf(
+            "%s %s dev/tasks/RunWebconsoleTask task=%d &> /dev/null &",
             PHP_BINARY,
             escapeshellarg($cliScriptPath),
             $task->ID
         );
 
-        // Execute outer command
-        exec(sprintf(
-            "%s &> /dev/null &",
-            $shellCMD
-        ));
+        // Create background task and run
+        $descriptors = array(
+            0 => array('pipe', 'r'), // STDIN
+            1 => array('pipe', 'w'), // STDOUT
+            2 => array('pipe', 'w')  // STDERR
+        );
+        $envs = array_merge($_SERVER, $_ENV);
+
+        // Execute and close
+        $process = proc_open(
+            $runnerCommand,
+            $descriptors,
+            $pipes,
+            BASE_PATH,
+            $envs
+        );
+        if (!is_resource($process)) {
+            throw new Exception("Could not run task");
+        }
+        
+        fclose($pipes[0]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        proc_close($process);
 
         return $task;
     }
